f6dd2022de12f1ed10b912a8e7992b9030bf3f04
Mahesh Babu <mmadala@GDCWFDT800316.igategegdc.com>
2016-09-22 12:53:12 +0530

    Testing

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	gitcommit: {
+		your_target: {
+		  options: {
+			message: "Testing",
+			noVerify: true,
+			noStatus: false
+		  },
+		  files: [
+			{
+			  src: ["Gruntfile.js"]
+			}
+		  ]
+		}
+	},
+	
+	gitadd: {
+		task: {
+		  options: {
+			force: true
+		  },
+		  files: {
+			src: ["Gruntfile.js"]
+		  }
+		}
+	}
+	
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-git');
+  
+  grunt.registerTask('setup', ['gitadd','gitcommit']);
+
+};
